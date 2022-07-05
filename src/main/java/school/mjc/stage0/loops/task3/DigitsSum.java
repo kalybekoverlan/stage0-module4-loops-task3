4d984b686bd491c0390196e0c8c6bd6e840b4f0d
erlan <kalybekov.erlan@gmail.com>
2022-07-05 22:11:02 +0600

    module 4 task 3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,5 +3,14 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
 
+        String textInteger = "" + t;
+        int sum = 0;
+
+        for(int i = 0; i < textInteger.length(); i++) {
+            if(textInteger.charAt(i) == '-')
+                continue;
+           sum = sum + Integer.parseInt("" + textInteger.charAt(i));
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int fib = 0 ;
+        int fib1 = 0, fib2 =1;
+
+        System.out.println(0);
+        System.out.println(1);
+        for(int i = 2; i < lastFibonacci; i++) {
+            fib = fib1 + fib2;
+            System.out.println(fib);
+            fib1 = fib2;
+            fib2 = fib;
+        }
+
     }
 }
